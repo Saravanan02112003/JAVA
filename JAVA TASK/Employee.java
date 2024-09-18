6926451530201768908c9da397c35710a3a87a1e
Saravanan M <romansaravanan143@gmail.com>
2024-09-18 18:27:16 +0530

    Add files via upload

---
+++ JAVA TASK/Employee.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Employee {
+	public static void main(String args[]) {
+		
+		
+		int age=91;
+		if(age<90)
+		{
+			System.out.println("you are eligible the job ");
+			
+		}
+		else {
+			System.out.println("you are not eligible this job");
+		}
+		}
+
+
+}
---
+++ JAVA TASK/PossNegazero.java
@@ -0,0 +1,21 @@
+package task;
+
+public class PossNegazero {
+
+	public static void main(String[] args) {
+		int n=-1;
+		if(n==0)
+		{
+			System.out.println("zero");
+		}
+			else if(n<0) {
+				System.out.println("negative");
+				
+		}
+		else {
+			System.out.println("possible");
+		}
+
+	}
+
+}
