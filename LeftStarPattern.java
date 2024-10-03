a7666cd102f54ef4724800ed4cfcd7af45531f7f
Saravanan M <romansaravanan143@gmail.com>
2024-10-03 10:35:17 +0530

    Add files via upload

---
+++ LeftStarPattern.java
@@ -0,0 +1,19 @@
+package first;
+
+public class LeftStarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,row=6;
+		for(i=1;i<row;i++)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print("");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
